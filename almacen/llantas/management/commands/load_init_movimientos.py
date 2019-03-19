8eda3ee5e80c49cfcdee3dd8f8eca9a54ca92aff
ramirovaz <r.vazquez@meditecsols.com>
2019-03-19 08:01:55 -0600

    load init working in command

--- almacen/llantas/management/commands/load_init_movimientos.py
+++ almacen/llantas/management/commands/load_init_movimientos.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils import timezone
 
+from llantas.utils import *
 from llantas.models import *
 from persona.models import Profile
 
@@ -12,25 +13,18 @@ class Command(BaseCommand):
     help = 'Load init movimientos from CSV.'
     def handle(self, *args, **options):
         
-        def return_user(username):
-            try:
-                u = User.objects.get(username=username)
-            except User.DoesNotExist:
-                u = User.objects.create_user(username, '{}@fletesexpress.com.mx'.format(username), 'esteesunpasswordtest785412')
-            return u
-
         ## valores fijos
         fecha_vale = datetime.datetime.strptime('04/02/2019', "%d/%m/%Y").date()
-        persona_asociada = return_user('rvazquez')
-        pa, pa_flag = Profile.objects.get_or_create(user=persona_asociada)
+        pa = return_profile('rvazquez')        
         no_folio = "XXXX"
-        tm, tm_flag = TipoMovimiento.objects.get_or_create(nombre="ENTRADA")
+        tme, tme_flag = TipoMovimiento.objects.get_or_create(nombre="ENTRADA")
 
         vale_inicial, vale_inicial_flag = Vale.objects.get_or_create(
             no_folio=no_folio, 
-            tipo_movimiento=tm,
+            tipo_movimiento=tme,
             fecha_vale=fecha_vale,
             persona_asociada=pa,
+            creador_vale=pa,
             defaults={"observaciones_grales":"Carga inicial de el inventario."})
 
 
@@ -38,46 +32,55 @@ class Command(BaseCommand):
             readCSV = csv.reader(csvfile_in, delimiter=';')
             for indice, row in enumerate(readCSV):
                 if indice != 0: # quit name of column
-                    tipo_movimiento  = row[0].strip()
+
+                    tipo_movimiento  = row[0].strip().capitalize()
                     f_movimiento = row[1].strip()
                     fecha_movimiento = datetime.datetime.strptime(f_movimiento, "%d/%m/%Y").date()
                     no_folio         = row[2].strip()
 
                     origen           = row[3].strip()
                     destino          = row[4].strip()
-                                      ## SKU
+                    
+                    ## GENERAL LA LLANTA
                     marca            = row[6].strip().capitalize()
                     medida           = row[7].strip().upper()
                     posicion         = row[8].strip().upper()
-                    cantidad         = row[9].strip()
                     status           = row[10].strip().capitalize()
                     dot              = row[11].strip()
+
+                    cantidad         = row[9].strip()
                     precio_unitario  = row[12]
                     if not precio_unitario:
                         precio_unitario = 0.0
 
 
-                    usuario_origen = return_user(origen)
-                    usuario_destino = return_user(destino)
-                    po , po_flag = Profile.objects.get_or_create(user=usuario_origen)
-                    pd, pd_flag = Profile.objects.get_or_create(user=usuario_destino)
+
+                    profile_origen = return_profile(origen, "ABSTRACT")
+                    profile_destino = return_profile(destino, "BODEGA")
+
+                    # Lllanta
                     m, m_flag = Marca.objects.get_or_create(nombre=marca)
                     me, me_flag = Medida.objects.get_or_create(nombre=medida)
                     pos, pos_flag = Posicion.objects.get_or_create(nombre=posicion)
                     sta, sta_flag = Status.objects.get_or_create(nombre=status)
+                    # dot
 
-                    movimiento = Movimiento(
-                        vale=vale_inicial,
-                        fecha_movimiento = vale_inicial.fecha_vale, 
-                        tipo_movimiento=vale_inicial.tipo_movimiento,
-                        origen=po,
-                        destino=pd,
+                    llanta, llanta_flag = Llanta.objects.get_or_create(
                         marca=m,
                         medida=me,
                         posicion=pos,
-                        cantidad=cantidad,
                         status=sta,
-                        dot=dot,
+                        dot=dot
+                    )
+
+                    movimiento = Movimiento(
+                        vale=vale_inicial,
+                        tipo_movimiento=vale_inicial.tipo_movimiento,
+                        fecha_movimiento = vale_inicial.fecha_vale, 
+                        origen=profile_origen,
+                        destino=profile_destino,
+                        llanta=llanta,
+                        cantidad=cantidad,
                         precio_unitario=precio_unitario,
                         creador=vale_inicial.persona_asociada, 
                     )
