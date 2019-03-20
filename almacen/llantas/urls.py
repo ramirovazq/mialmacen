bd2a60b81b99ae1e32d01360e106790e98970517
ramirovaz <r.vazquez@meditecsols.com>
2019-03-20 04:55:42 -0600

    can edit a vale salida

--- almacen/llantas/urls.py
+++ almacen/llantas/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', movimientos, name='movimientos'),
     path('salidas/', salidas, name='salidas'),
     path('salida/', salida, name='salida'),
+    path('salida/<int:vale_id>/edit/', salida_edit, name='salida_edit'),
     path('salida/<int:vale_id>/', salida_add, name='salida_add'),
     path('salida/<int:vale_id>/impresion/', salida_impresion, name='salida_impresion'),
     path('salida/<int:vale_id>/movimiento/', salida_add_movimiento, name='salida_add_movimiento'),
