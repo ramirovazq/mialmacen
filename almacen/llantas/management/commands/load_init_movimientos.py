e80c22e5efe9cd77c2a5aad21473536f48f11f59
ramirovaz <r.vazquez@meditecsols.com>
2019-03-20 02:21:41 -0600

    not static vale in command, from csv

--- almacen/llantas/management/commands/load_init_movimientos.py
+++ almacen/llantas/management/commands/load_init_movimientos.py
@@ -13,27 +13,14 @@ class Command(BaseCommand):
     help = 'Load init movimientos from CSV.'
     def handle(self, *args, **options):
         
-        ## valores fijos
-        fecha_vale = datetime.datetime.strptime('04/02/2019', "%d/%m/%Y").date()
-        pa = return_profile('rvazquez')        
-        no_folio = "XXXX"
-        tme, tme_flag = TipoMovimiento.objects.get_or_create(nombre="ENTRADA")
-
-        vale_inicial, vale_inicial_flag = Vale.objects.get_or_create(
-            no_folio=no_folio, 
-            tipo_movimiento=tme,
-            fecha_vale=fecha_vale,
-            persona_asociada=pa,
-            creador_vale=pa,
-            defaults={"observaciones_grales":"Carga inicial de el inventario."})
-
+        pa = return_profile('rvazquez')# valor fijo
 
         with open(settings.BASE_DIR + '/almacen/load_init/movimientos.csv') as csvfile_in:
             readCSV = csv.reader(csvfile_in, delimiter=';')
             for indice, row in enumerate(readCSV):
                 if indice != 0: # quit name of column
 
-                    tipo_movimiento  = row[0].strip().capitalize()
+                    tipo_movimiento  = row[0].strip().upper() # ENTRADA SALIDA ## mayusculas
                     f_movimiento = row[1].strip()
                     fecha_movimiento = datetime.datetime.strptime(f_movimiento, "%d/%m/%Y").date()
                     no_folio         = row[2].strip()
@@ -42,17 +29,36 @@ class Command(BaseCommand):
                     destino          = row[4].strip()
                     
                     ## GENERAL LA LLANTA
-                    marca            = row[6].strip().capitalize()
-                    medida           = row[7].strip().upper()
-                    posicion         = row[8].strip().upper()
-                    status           = row[10].strip().capitalize()
-                    dot              = row[11].strip()
+                    marca            = row[6].strip().capitalize() # Solo primera letra mayuscula
+                    medida           = row[7].strip().upper() ## mayusculas
+                    posicion         = row[8].strip().upper() ## mayusculas
+                    status           = row[10].strip().capitalize() # Solo primera letra mayuscula
+                    dot              = row[11].strip() ## es numerico
 
                     cantidad         = row[9].strip()
                     precio_unitario  = row[12]
                     if not precio_unitario:
                         precio_unitario = 0.0
 
+
+                    ## valores del Vale
+                    
+                    #no_folio = "XXXX"                    
+                    fecha_vale = datetime.datetime.strptime(f_movimiento, "%d/%m/%Y").date()                    
+                    tme, tme_flag = TipoMovimiento.objects.get_or_create(nombre=tipo_movimiento)
+
+                    vale_inicial, vale_inicial_flag = Vale.objects.get_or_create(
+                        no_folio=no_folio, 
+                        tipo_movimiento=tme,
+                        fecha_vale=fecha_vale,
+                        persona_asociada=pa,
+                        creador_vale=pa,
+                        defaults={"observaciones_grales":"Carga inicial de el inventario."})
+
+                    if vale_inicial_flag:
+                        self.stdout.write(self.style.SUCCESS('Se genero el vale #{}'.format(no_folio)))
+                    else:
+                        self.stdout.write(self.style.ERROR('Ya estaba generado el vale #{}'.format(no_folio)))
 
 
                     profile_origen = return_profile(origen, "ABSTRACT")
