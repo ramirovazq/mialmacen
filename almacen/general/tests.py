ab0637c8164af5f8578798316f9f93354673bdbb
Ramiro Vazquez <ramirovazquez95@gmail.com>
2019-11-02 19:50:41 -0600

    test por specific places and movements

--- almacen/general/tests.py
+++ almacen/general/tests.py
@@ -803,3 +803,278 @@ class ProductosMovimientoPositionTestCase(TestCase):
         self.assertEqual(exactposition_cable_cuatro_inventory["ALMACEN_GENERAL>>Anaquel 1>>Nivel de Anaquel 1"], 5.7)
         self.assertEqual(exactposition_cable_cuatro_inventory["ALMACEN_GENERAL>>Anaquel 1>>Nivel de Anaquel 6"], 623.0)
         self.assertEqual(exactposition_cable_cuatro_inventory["ALMACEN_ALTERNO>>Anaquel 11>>Nivel de Anaquel 24"], 12.0)
+
+
+
+
+class ProductosExactPositionTestCase(TestCase):
+
+    def setUp(self):
+
+        self.user01 = return_profile("rosa")
+        self.user02 = return_profile("goyo")
+
+        self.conteo = return_profile("CONTEO", "ABSTRACT")
+        self.bodega01 = return_profile("ALMACEN_GENERAL", "BODEGA")
+        self.tractor01 = return_profile("TRACTOR01", "ECONOMICO")
+
+        anaquel_one = Position.objects.create(name="Anaquel 1")
+
+        nivel_one            = Position.objects.create(name="Nivel de Anaquel 1", parent=anaquel_one)
+        nivel_twenty_three   = Position.objects.create(name="Nivel de Anaquel 23", parent=anaquel_one)
+
+        self.bodega01_nivel_one = ProfilePosition.objects.create(profile=self.bodega01, position=nivel_one)
+        self.bodega01_nivel_twenty_three = ProfilePosition.objects.create(profile=self.bodega01, position=nivel_twenty_three)
+
+        self.tm_entrada = TipoMovimiento.objects.create(nombre="ENTRADA")
+        self.tm_salida  = TipoMovimiento.objects.create(nombre="SALIDA")
+
+        tipo_smaller,   bandera = TipoUnidadMedida.objects.get_or_create(tipo="-1")
+        tipo_reference, bandera = TipoUnidadMedida.objects.get_or_create(tipo="0")
+        tipo_greater,   bandera = TipoUnidadMedida.objects.get_or_create(tipo="1")
+
+        categoria_longitud, bandera = CategoriaUnidadMedida.objects.get_or_create(nombre="Longitud/Distancia")
+        categoria_unidad,   bandera = CategoriaUnidadMedida.objects.get_or_create(nombre="Unidad")
+
+        self.fourfeb = datetime.datetime.strptime('04/02/2019', "%d/%m/%Y").date()
+
+        self.unidad = UnidadMedida.objects.create(
+            nombre="unidad",
+            categoria=categoria_unidad,
+            tipo_unidad=tipo_reference,
+            ratio=1,
+            simbolo="m"
+            )
+
+        self.vale01 = ValeAlmacenGeneral.objects.create(
+                no_folio="0001",
+                observaciones_grales="nada",
+                tipo_movimiento=self.tm_entrada,
+                fecha_vale=self.fourfeb,
+                persona_asociada=self.user02, # quien entrega
+                creador_vale=self.user01,
+            )
+
+        self.filtro_aire = Producto.objects.create(nombre="Filtros de aire")
+        self.producto_x = Producto.objects.create(nombre="Producto X")
+
+        self.vale02 = ValeAlmacenGeneral.objects.create(
+                no_folio="0002",
+                observaciones_grales="segunda entrada",
+                tipo_movimiento=self.tm_entrada,
+                fecha_vale=self.fourfeb,
+                persona_asociada=self.user02, # quien entrega
+                creador_vale=self.user01,
+            )
+
+        self.vale03 = ValeAlmacenGeneral.objects.create(
+                no_folio="0003",
+                observaciones_grales="el 3ero de entrada",
+                tipo_movimiento=self.tm_entrada,
+                fecha_vale=self.fourfeb,
+                persona_asociada=self.user02, # quien entrega
+                creador_vale=self.user01,
+            )
+
+
+        self.vale04 = ValeAlmacenGeneral.objects.create(
+                no_folio="0004",
+                observaciones_grales="el de salida",
+                tipo_movimiento=self.tm_salida,
+                fecha_vale=self.fourfeb,
+                persona_asociada=self.user02, # quien entrega
+                creador_vale=self.user01,
+            )
+
+        self.vale05 = ValeAlmacenGeneral.objects.create(
+                no_folio="0005",
+                observaciones_grales="el de salida",
+                tipo_movimiento=self.tm_salida,
+                fecha_vale=self.fourfeb,
+                persona_asociada=self.user02, # quien entrega
+                creador_vale=self.user01,
+            )
+
+
+
+    def test_three_entries_one_warehouse(self):
+
+        #vale01 # bodega01 # ENTRADA # nivel one #filtro_aire  12 
+        other_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale01,
+            tipo_movimiento=self.vale01.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.producto_x,
+            unidad=self.unidad,
+            cantidad=500, 
+            precio_unitario=150.5,
+            creador=self.user01,
+            observacion="otro movimiento"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one,
+            movimiento=other_mov_entrada
+        )
+
+
+        #vale01 # bodega01 # ENTRADA # nivel one #filtro_aire  12 
+        zero_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale01,
+            tipo_movimiento=self.vale01.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=12, 
+            precio_unitario=150.5,
+            creador=self.user01,
+            observacion="1er movimiento"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one,
+            movimiento=zero_mov_entrada
+        )
+
+        #vale01 # bodega01 # ENTRADA # nivel_one #filtro_aire 8  ( 12 + 8 )
+        primer_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale01,
+            tipo_movimiento=self.vale01.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=8, # s metros
+            precio_unitario=135.5,
+            creador=self.user01,
+            observacion="2do movimiento"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one,
+            movimiento=primer_mov_entrada
+        )
+
+
+        #vale02 # bodega01 # ENTRADA # nivel_one #filtro_aire  15 ( 12 + 8 + 15 )
+        segundo_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale02,
+            tipo_movimiento=self.vale02.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=15, # s metros
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="er movimiento"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one,
+            movimiento=segundo_mov_entrada
+        )
+
+
+        #vale03 # bodega01 # ENTRADA # nivel_one #filtro_aire  100  (12 + 8 + 15 + 100 = 135 )
+        tercero_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale03,
+            tipo_movimiento=self.vale03.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=100, # 
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="entrada 3er vale"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one,
+            movimiento=tercero_mov_entrada
+        )
+
+        #vale03 # bodega01 # ENTRADA # nivel_twenty_three #filtro_aire  23
+        cuarto_mov_entrada = MovimientoGeneral.objects.create(
+            vale=self.vale03,
+            tipo_movimiento=self.vale03.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.conteo,            
+            destino=self.bodega01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=23, # 
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="entrada 3er vale"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_twenty_three, #nivel_twenty_three
+            movimiento=cuarto_mov_entrada
+        )
+
+
+        #vale04 # bodega01 # SALIDA # nivel_one #filtro_aire  5  (12 + 8 + 15 + 100 = 135 - 5 = 130)
+        quinto_mov_salida = MovimientoGeneral.objects.create( 
+            vale=self.vale04,
+            tipo_movimiento=self.vale04.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.bodega01,            
+            destino=self.tractor01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=5, # 
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="salida 5to mov"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_one, #nivel_one
+            movimiento=quinto_mov_salida
+        )
+
+        #vale04 # bodega01 # SALIDA # nivel_twenty_three #filtro_aire  5 ( 23 - 3 )
+        sexto_mov_salida = MovimientoGeneral.objects.create(
+            vale=self.vale04,
+            tipo_movimiento=self.vale04.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.bodega01,            
+            destino=self.tractor01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=3, # 
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="salida 5to mov"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_twenty_three, #nivel_twenty_three
+            movimiento=sexto_mov_salida
+        )
+
+        #vale05 # bodega01 # SALIDA # nivel_twenty_three #filtro_aire  7 ( 23 - 3 - 7 = 13 )
+        septimo_mov_salida = MovimientoGeneral.objects.create(
+            vale=self.vale05,
+            tipo_movimiento=self.vale05.tipo_movimiento,
+            fecha_movimiento=self.fourfeb,            
+            origen=self.bodega01,            
+            destino=self.tractor01,            
+            producto=self.filtro_aire,
+            unidad=self.unidad,
+            cantidad=7, # 
+            precio_unitario=10.5,
+            creador=self.user01,
+            observacion="ultima salida 5to mov"
+        )
+        ProductoExactProfilePosition.objects.create(
+            exactposition=self.bodega01_nivel_twenty_three, #nivel_twenty_three
+            movimiento=septimo_mov_salida
+        )
+
+
+        exactposition_filtro = self.filtro_aire.what_in_positions_inventory_specific()
+        self.assertEqual(exactposition_filtro['ALMACEN_GENERAL>>Anaquel 1>>Nivel de Anaquel 1'], float(130))
+        self.assertEqual(exactposition_filtro['ALMACEN_GENERAL>>Anaquel 1>>Nivel de Anaquel 23'], float(13))
