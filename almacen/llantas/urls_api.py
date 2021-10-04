c9d5851d3f2830e7389cd64754ee41a2710cacc6
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2021-10-04 18:41:41 -0500

    enable endpoints

--- almacen/llantas/urls_api.py
+++ almacen/llantas/urls_api.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from llantas.views_api import ValeViewSet, LlantaViewSet, EconomicoViewSet, MovimientoViewSet
 from general.views_api import ProductoViewSet
-from persona.views_api import ProfilePositionViewSet
+from persona.views_api import ProfilePositionViewSet, ProfileBodegaViewSet, ProfileEconomicoViewSet
 
 router = DefaultRouter()
 router.register(r'vale', ValeViewSet, basename='vale')
@@ -10,4 +10,6 @@ router.register(r'economico', EconomicoViewSet, basename='economico')
 router.register(r'movimiento', MovimientoViewSet, basename='movimiento')
 router.register(r'producto', ProductoViewSet, basename='producto')
 router.register(r'profileposition', ProfilePositionViewSet, basename='profileposition')
+router.register(r'profilebodega', ProfileBodegaViewSet, basename='profilebodega')
+router.register(r'profileeconomico', ProfileEconomicoViewSet, basename='profileeconomico')
 urlpatterns = router.urls
--- almacen/persona/serializers.py
+++ almacen/persona/serializers.py
@@ -1,23 +1,15 @@
 from rest_framework import serializers
-from persona.models import ProfilePosition
+from persona.models import ProfilePosition, Profile
 
 class ProfilePositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfilePosition
         fields = ['id'] #, 'profile', 'position']
 
-'''
-from rest_framework import serializers
-from persona.models import ProfilePosition
-class ProfilePositionSerializer(serializers.ModelSerializer):
-     class Meta:
-         model = ProfilePosition
-         fields = ['id']
-         #fields = ['id', 'profile', 'position']
-pp = ProfilePosition.objects.all()[0]
-s = ProfilePositionSerializer(pp)
-s.data
-{'id': 1, 'profile': 144, 'position': 2}
-ProfilePositionSerializer(ProfilePosition.objects.all(), many=True)
-ids_pp = ProfilePosition.objects.all().values_list('id')
-'''
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    tipo = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'tipo']
+
